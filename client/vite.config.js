4b8bb77bf0af286a784e105564678dc28e0a6eaa
Typebrozen <netajibose1900@gmail.com>
2026-07-12 06:32:38 +0530

    fix old device compatibility

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
     port: 5173,
     proxy: {
@@ -13,4 +16,8 @@ export default defineConfig({
       },
     },
   },
-});
+  build: {
+    target: 'es2015',
+    cssTarget: 'chrome61',
+  },
+})
